385b825255b347842cb1d2160a3f3ef39800c83e
maks-ushakov <ush.maksim@gmail.com>
2021-04-25 13:06:52 +0300

    Add runner to autoload

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'Hello, Hexlet!';
+require_once __DIR__ . '/vendor/autoload.php';
+
+// File does not included directly
+// It load automatically because Composer autoload works )))
+
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
